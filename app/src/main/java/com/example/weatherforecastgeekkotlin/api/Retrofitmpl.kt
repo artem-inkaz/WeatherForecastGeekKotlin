e1d53d36ddca8fd769b22087ecc75dbb9000925b
artemternovenko <artem.inkaz@gmail.com>
2021-02-24 19:55:32 +0600

    [ADDED] forecast var1

--- app/src/main/java/com/example/weatherforecastgeekkotlin/api/Retrofitmpl.kt
+++ app/src/main/java/com/example/weatherforecastgeekkotlin/api/Retrofitmpl.kt
@@ -1,24 +1,45 @@
 package com.example.weatherforecastgeekkotlin.api
 
-import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-
-
+import java.io.IOException
 
 class Retrofitmpl {
-    fun getWeatherApi(): WeatherAPI{
-        val retrofit: Retrofit = Retrofit.Builder()
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(
-                GsonConverterFactory.create(
-                    GsonBuilder().setLenient().create()
+        fun getWeatherApi(
+//            interceptor: CommonInterceptor,
+//            loggingInterceptor: LoggingInterceptor
+        ): WeatherAPI {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("https://api.weather.yandex.ru/")
+//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create(
+//                        GsonBuilder().setLenient().create()
+                    )
                 )
-            )
-            .baseUrl("https://api.weather.yandex.ru/v2/")
-            .build()
-        return retrofit.create(WeatherAPI::class.java)
+                .client(createOkHttpClient(CommonInterceptor()))
+                .build()
+            return retrofit.create(WeatherAPI::class.java)
+        }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        return httpClient.build()
+    }
+
+    inner class CommonInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            val request = chain.request()
+                .newBuilder()
+                .addHeader("X-Yandex-API-Key",  "bf3d16a4-e51f-4c90-b9bd-23bb1f52ffc7")
+                .build()
+            return chain.proceed(request)
+        }
     }
 }
---
+++ app/src/main/java/com/example/weatherforecastgeekkotlin/openweathermap/models/DataModelMap.kt
@@ -0,0 +1,3 @@
+package com.example.weatherforecastgeekkotlin.openweathermap.models
+
+data class DataModelMap()
